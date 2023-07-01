2edbbe2fb939f330968f76de607e31bdaa72d999
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-02 00:34:14 +0500

    conditional statements

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  title: string = 'Register Component';
+  // show: boolean = true;
+  userName: string = 'Nabeel'
 }
